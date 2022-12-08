faea35e55eedf9277f087ef473692cdac0471377
yoyolon <yoyograph74@gmail.com>
2022-12-08 20:59:06 +0900

    [fix] 軽微な修正

--- src/config.py
+++ src/config.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 
 # matplotlib
 plt.rcParams['lines.color'] = 'white'
-plt.rcParams['patch.edgecolor'] = 'white'
 plt.rcParams['text.color'] = 'white'
+plt.rcParams['figure.facecolor'] = '#333333'
+plt.rcParams['figure.edgecolor'] = '#333333'
 plt.rcParams['axes.facecolor'] = '#333333'
 plt.rcParams['axes.edgecolor'] = 'white'
 plt.rcParams['axes.labelcolor'] = 'white'
 plt.rcParams['xtick.color'] = 'white'
 plt.rcParams['ytick.color'] = 'white'
 plt.rcParams['grid.color'] = 'white'
-plt.rcParams['figure.facecolor'] = '#333333'
-plt.rcParams['figure.edgecolor'] = '#333333'
 plt.rcParams['savefig.facecolor'] = '#333333'
 plt.rcParams['savefig.edgecolor'] = '#333333'
+plt.rcParams['legend.edgecolor'] = '#333333'
--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ import sv_ttk
 
 if __name__=="__main__":
     window = tk.Tk() 
-    sv_ttk.set_theme("dark")
     app = App(window)
+    sv_ttk.set_theme("dark")
     app.mainloop()
